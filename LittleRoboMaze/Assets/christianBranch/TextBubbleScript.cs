a6eff4d064516fe48cd6d6d5b1db03d8da76bdcc
Christian Sumares <christiansumares@gmail.com>
2020-02-14 15:52:14 -0800

    Imported TMP Essentials

---
+++ LittleRoboMaze/Assets/christianBranch/TextBubbleScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBubbleScript : MonoBehaviour {
+
+    public void ShowBubble(string text, float duration) {
+        Transform camera = GameObject.Find("Main Camera").transform;
+        GameObject textBubble = new GameObject("Text Bubble");
+        textBubble.transform.LookAt(camera);
+
+    }
+
+}
