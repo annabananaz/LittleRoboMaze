e197602aec522447267ed9695fee91cf359995ee
Christian Sumares <christiansumares@gmail.com>
2020-02-14 16:55:46 -0800

    First version of text bubbles

--- LittleRoboMaze/Assets/christianBranch/TextBubbleScript.cs
+++ LittleRoboMaze/Assets/christianBranch/TextBubbleScript.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextBubbleScript : MonoBehaviour {
 
-    public void ShowBubble(string text, float duration) {
-        Transform camera = GameObject.Find("Main Camera").transform;
-        GameObject textBubble = new GameObject("Text Bubble");
-        textBubble.transform.LookAt(camera);
+    public GameObject textBubblePrefab;
 
+    public void ShowBubble(string text, float duration) {
+        StartCoroutine(ShowBubbleEnumerator(text, duration));
     }
 
+    private IEnumerator ShowBubbleEnumerator(string text, float duration) {
+        Transform camera = GameObject.Find("Main Camera").transform;
+        Transform player = gameObject.transform;
+        GameObject textBubbleObj = Instantiate(textBubblePrefab, player.position + Vector3.up * 3, Quaternion.identity);
+        textBubbleObj.transform.LookAt(camera.position);
+        textBubbleObj.transform.Rotate(0, 180, 0);
+        TextMeshPro tmp = textBubbleObj.GetComponent<TextMeshPro>();
+        tmp.text = text;
+        yield return new WaitForSeconds(duration);
+        if (duration > 0)
+            Destroy(textBubbleObj);
+    }
 }
---
+++ LittleRoboMaze/Assets/christianBranch/TextBubbleTester.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBubbleTester : MonoBehaviour {
+    void Start() {
+        GameObject.Find("Player").GetComponent<TextBubbleScript>().ShowBubble("Hello, World", 5);
+    }
+}
