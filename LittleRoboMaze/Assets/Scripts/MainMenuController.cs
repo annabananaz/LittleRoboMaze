3674d21bc3e455e0b4cd77c1986d10893cd260bf
Eric Gonzalez Olivares <gonz9019@csumb.ad.csumb.edu>
2020-03-10 21:51:45 -0700

    Created the main menu

---
+++ LittleRoboMaze/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button startButton;
+    public Button quitButton;
+    public Text loadText;
+    public float loadTime;
+
+    private float timePercent;
+    private bool loadingDone;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(QuitGame);
+        loadText.text = "";
+        timePercent = 0.0f;
+        loadingDone = false;
+    }
+
+    void StartGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadScene()
+    {
+        float timer = 0.0f;
+        while(timer < loadTime)
+        {
+            timer += Time.deltaTime;
+            float value = timer / loadTime;
+            timePercent = Mathf.Lerp(0.0f, 1.0f, value);
+            yield return null;
+        }
+
+    }
+
+    private void Update()
+    {
+        //Debug.Log(timePercent);
+        if (!loadingDone)
+        {
+            if (timePercent >= 1)
+                loadingDone = true;
+            else if (timePercent >= 0.75f)
+                loadText.text = "Scene built. Loading now.";
+            else if (timePercent > 0.5f)
+                loadText.text = "Building program . . .";
+            else if (timePercent > 0.0f)
+                loadText.text = "Compiling code . . .";
+        }
+        else
+        {
+
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
